2be82b3b25872d0c321be9b6bfe00925c3b30f64
Timothy Pulliam <timothy.dpulliam@gmail.com>
2021-12-24 16:38:06 -0500

    allow 2 or 4 azs

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-  terraform {
+terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -14,7 +14,7 @@
 
 provider "aws" {
   # Configuration options
-  region = local.region
+  region = var.region
 }
 
 
@@ -30,9 +30,33 @@ variable "vpc_name" {
   default = "test_vpc"
 }
 
+variable "vpc_cidr" {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "num_azs" {
+  type = number
+  description = "number of availability zones the VPC is spread across"
+  default = 2
+
+  validation {
+    condition     = var.num_azs != 2 || var.num_azs != 4
+    error_message = "The VPC can only be split across 2 or 4 Availability Zones."
+  }
+}
+
+locals {
+  description = "list of availability zones"
+  suffixes = ["a", "b", "c", "d"]
+
+  azs = slice([for s in local.suffixes : "${var.region}${s}"], 0, var.num_azs)
+  subnets = (var.num_azs == 2 ? cidrsubnets(var.vpc_cidr, 2, 2, 3, 3, 4, 4) : cidrsubnets(var.vpc_cidr, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5))
+}
+
 
 resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
   instance_tenancy = "default"
 
   // If both attributes are enabled, an instance launched into the 
@@ -59,8 +83,8 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_subnet" "privateSubnet1A" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.0.0/18"
-  availability_zone = "${local.region}a"
+  cidr_block = local.subnets[0] //"10.0.0.0/18"
+  availability_zone = local.azs[0]
 
   tags = {
     Name = "privateSubnet1A"
@@ -69,8 +93,8 @@ resource "aws_subnet" "privateSubnet1A" {
 
 resource "aws_subnet" "privateSubnet2A" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.64.0/18"
-  availability_zone = "${local.region}b"
+  cidr_block = local.subnets[1] //"10.0.64.0/18"
+  availability_zone = local.azs[1]
 
   tags = {
     Name = "privateSubnet2A"
@@ -79,8 +103,8 @@ resource "aws_subnet" "privateSubnet2A" {
 
 resource "aws_subnet" "publicSubnet1" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.128.0/19"
-  availability_zone = "${local.region}a"
+  cidr_block = local.subnets[2] //"10.0.128.0/19"
+  availability_zone = local.azs[0]
   map_public_ip_on_launch = true
 
   tags = {
@@ -90,8 +114,8 @@ resource "aws_subnet" "publicSubnet1" {
 
 resource "aws_subnet" "publicSubnet2" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.160.0/19"
-  availability_zone = "${local.region}b"
+  cidr_block = local.subnets[3] //"10.0.160.0/19"
+  availability_zone = local.azs[1]
   map_public_ip_on_launch = true
 
   tags = {
@@ -101,8 +125,8 @@ resource "aws_subnet" "publicSubnet2" {
 
 resource "aws_subnet" "privateSubnet1B" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.192.0/20"
-  availability_zone = "${local.region}a"
+  cidr_block = local.subnets[4] //"10.0.192.0/20"
+  availability_zone = local.azs[0]
 
   tags = {
     Name = "privateSubnet1B"
@@ -111,8 +135,8 @@ resource "aws_subnet" "privateSubnet1B" {
 
 resource "aws_subnet" "privateSubnet2B" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.208.0/20"
-  availability_zone = "${local.region}b"
+  cidr_block = local.subnets[5] //"10.0.208.0/20"
+  availability_zone = local.azs[1]
 
   tags = {
     Name = "privateSubnet2B"
